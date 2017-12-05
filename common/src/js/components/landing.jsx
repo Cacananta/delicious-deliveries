8b789aefc70ea67f453b71aed0a5ec171952f577
Brandon Benoit <b.nutty@gmail.com>
2017-12-05 11:43:20 -0800

    added current code to landing component

--- common/src/js/components/landing.jsx
+++ common/src/js/components/landing.jsx
@@ -1,34 +1,66 @@
 import React, { Component } from 'react';
 
-const background = {
-    backgroundImage: 'url(./DD-landing.jpg)',
-    height: '500px',
-    width: '200px',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover'
-};
-
 export default class Landing extends Component {
     render() {
         return (
-            <div>
+            <div className='container'>
                 <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light mb-5">
                     <a className='navbar-brand order-1 font-weight-bold' href="#">Login</a>
-                    <a className="navbar-brand order-1 font-weight-bold float-right" href='#'>Sign-Up</a>
-                    <button className="navbar-toggler" type='button' data-toggle='collapse' data-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <a className="navbar-brand order-2 font-weight-bold" href='#'>Sign-Up</a>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-item nav-link" href="/">Delicious Deliveries</a>
+                            <a className="nav-item nav-link font-weight-bold" href="/">Delicious Deliveries</a>
                         </div>
                     </div>
                 </nav>
-                <div style={background}>
-
-                </div>
                 <br />
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-2"></div>
+                        <div className="col-lg-8 h1 text-center font-italic">Delicious Deliveries</div>
+                        <div className="col-lg-2"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-2"></div>
+                        <div className="col-lg-8">
+                            <div className="input-group">
+                                <input type="text" className="form-control"/>
+                                <span className="input-group-btn">
+                                    <button className="btn btn-primary" type='button'>Go!</button>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-lg-2"></div>
+                    </div>                                       
+                </div>
+                <nav className="navbar fixed-bottom navbar-light bg-light">
+                    <div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a href="#" className="nav-item">About Us</a>
+                            </li>
+                            <li className="nav-item float-right">
+                                <a href="#" className="nav-item">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
             
         );
