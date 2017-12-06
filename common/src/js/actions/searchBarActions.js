a0a42d1af94f40034839c2a7c71831e72acd88ea
Corey Cacananta <cacananta@gmail.com>
2017-12-06 10:10:02 -0800

    "getting my rebase up to speed to push"

--- common/src/js/actions/searchBarActions.js
+++ common/src/js/actions/searchBarActions.js
@@ -1,15 +1,9 @@
-<<<<<<< Updated upstream
-export const types = {
-    UPDATE_SEARCH_LOCATION: 'UPDATE_SEARCH_LOCATION'
-   };
-=======
 import axios from 'axios';
 
 export const types = {
     UPDATE_SEARCH_LOCATION: 'UPDATE_SEARCH_LOCATION'
    };
 
->>>>>>> Stashed changes
    export function updateSearchLocation(input){
     return {
      type: types.UPDATE_SEARCH_LOCATION,
