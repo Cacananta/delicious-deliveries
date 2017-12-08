678bebfce3b08832e28a895fda38c209dae2560d
Corey Cacananta <cacananta@gmail.com>
2017-12-07 18:30:06 -0800

    received access token

--- common/src/js/reducers/loginReducers.js
+++ common/src/js/reducers/loginReducers.js
@@ -1,30 +1,48 @@
 import { types } from '../actions/loginActions';
 
 const INITIAL_STATE = {
-    email: '',
-    password: '',
-    owner: ''
+    email: 'bob@bob.com',
+    password: 'bob0',
+    owner: 'false',
+    activeOwner: false,
+    activeCustomer: false,
+    currentOwnerId: '',
+    currentCustomerId: ''
 };
 
-export default function LoginReducer (state = INITIAL_STATE, action) {
+export default function LoginReducer(state = INITIAL_STATE, action) {
     const { type, payload } = action;
     switch (type) {
-        // case (types.LOGIN_AUTH + '_FULFILLED'): {
-        //     if (payload) {
-        //         return {
-        //             ...state,
-        //             email: payload.email,
-        //             password: payload.password,
-        //             owner: ''
-        //         };
-        //     } else {
-        //         return {
-        //             ...state,
-        //             owner: ''
-        //         };
-        //     }
-        //     break;
-        // }
+        case types.LOGIN_OWNER: {
+            if (payload) {
+                return {
+                    ...state,
+                    currentOwnerId: payload.userId,
+                    activeOwner: true
+                };
+            } else {
+                return {
+                    ...state,
+                    password: ''
+                };
+            }
+            break;
+        }
+        case types.LOGIN_CUSTOMER: {
+            if (payload) {
+                return {
+                    ...state,
+                    currentCustomerId: payload.userId,
+                    activeCustomer: true
+                }
+            } else {
+                return {
+                    ...state,
+                    password: ''
+                };
+            }
+            break;
+        }
 
         case (types.OWNER_TOGGLE_LOGIN): {
             return {
