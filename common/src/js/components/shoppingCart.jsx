2cca7335f382b14afd3fe9ec4a9f31bf37a18ba2
Brandon Benoit <b.nutty@gmail.com>
2017-12-10 22:59:59 -0800

    styling on shopping cart

--- common/src/js/components/shoppingCart.jsx
+++ common/src/js/components/shoppingCart.jsx
@@ -47,50 +47,72 @@ export default class ShoppingCart extends React.Component {
       let checkoutHref = '#/login';
       if(activeCustomer==true) checkoutHref= '#/checkout';
     return(
-      <div className='container'>
+      <div>
          <NavBar/>
-        <div className='mb-3'>
-          <h1 className='text-center'>Shopping Cart</h1>
-        </div>
-        <div className='w-75 row mx-auto'>
-          <div className='col-4 text-center'>
-            <strong>Items</strong>
-          </div>
-          <div className='col-4 text-center'>
-            <strong>Quantity</strong>
-          </div>
-          <div className='col-4 text-center'>
-            <strong>Totals</strong>
+         <div className="jumbotron jumbotron-fluid mb-0">
+          <div className="container text-center">
+            <h1 className="display-3">Shopping Cart</h1>
           </div>
         </div>
-        {shoppingCart.map((cartItem, index) =>
-        <div key={ index } className='w-75 row mx-auto py-1'>
-          <div className='col-4 text-center'>
-            { cartItem.menuItem }
-          </div>
-          <div className='col-4 text-center d-flex'>
-            <span id={ cartItem.id } className='mr-auto badge badge-pill badge-danger' onClick={ this.subtractQuantity }>-</span>
-            <span id={ cartItem.id } className='mr-auto badge badge-pill badge-success' onClick={ this.addQuantity }>+</span>
-            { cartItem.quantity }
-            <button id={ cartItem.id } className='btn btn-sm btn-danger ml-auto' onClick={ this.removeItem }>Remove</button>
-          </div>
-          <div className='col-4 text-center'>
-            ${ (cartItem.price * cartItem.quantity).toFixed(2) }
+        <div className="container-fluid shortbg">
+          <div className='row mb-5'>
+            <div className="col-12 mb-5">
+            <div className="card mt-5">
+              <div className="card-header">
+                <div className="row">
+                  <div className='col-4 text-center'>
+                    <strong>Items:</strong>
+                  </div>
+                  <div className='col-4 text-center'>
+                    <strong>Quantity:</strong>
+                  </div>
+                  <div className='col-4 text-center'>
+                    <strong>Totals:</strong>
+                  </div>
+                </div>
+              </div>
+              {shoppingCart.map((cartItem, index) =>
+              <div key={ index } className='row'>
+                <div className='col-4 text-center'>
+                  { cartItem.menuItem }
+                </div>
+                <div className='col-4 text-center d-flex'>
+                  <span id={ cartItem.id } className='mr-auto badge badge-pill badge-danger' onClick={ this.subtractQuantity }>-</span>
+                  <span id={ cartItem.id } className='mr-auto badge badge-pill badge-success' onClick={ this.addQuantity }>+</span>
+                  { cartItem.quantity }
+                  <button id={ cartItem.id } className='btn btn-sm btn-danger ml-auto' onClick={ this.removeItem }>Remove</button>
+                </div>
+                <div className='col-4 text-center'>
+                  ${ (cartItem.price * cartItem.quantity).toFixed(2) }
+                </div>
+              </div>
+              )}
+                <hr/>
+                <div className='row'>
+                  <div className='col-4 text-center'>
+                    <a className='btn btn-primary' href='#/restaurant/a' role='button'>Back to Menu</a>
+                  </div>
+                  <div className='col-4 text-center'>
+                    <a className='btn btn-primary' href={ checkoutHref } role='button'>Checkout</a>
+                  </div>
+                  <div>
+                    <span className='col-4'>Final Total: $<strong>{ total.toFixed(2) }</strong></span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        )}
-        <hr/>
-        <div className='w-75 row mx-auto'>
-          <div className='col-4'>
-          <a className='btn btn-primary' href='#/restaurant/a' role='button'>Back to Menu</a>
-          </div>
-          <div className='col-4'>
-            <a className='btn btn-primary' href={ checkoutHref } role='button'>Checkout</a>
-          </div>
-          <div>
-            <span className='col-4'>Final Total: $<strong>{ total.toFixed(2) }</strong></span>
-          </div>
-        </div>
+        <nav className="navbar navbar-expand-lg opacity fixed-bottom justify-content-center">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link font-weight-bold text-dark" href="#/about">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold text-dark" href="#/contact">Contact Us</a>
+            </li>
+          </ul>
+        </nav> 
       </div>
     )
   }
